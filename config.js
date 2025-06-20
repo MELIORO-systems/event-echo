2c6197e9e9dd5972b2bab2e0af5082cb0bc41be8
MELIORO-systems <pavel@melioro.cz>
2025-06-21 00:12:26 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+    pageTitle: "Mini-aplikace: Měřič nálady",
+    appTitle: "Měřič nálady",
+    appSubtitle: "Pomozte nám sledovat aktuální náladu v komunitě!",
+    buttonText: "Jak se teď cítíš?",
+    modalTitle: "Jak se teď cítíš?",
+    statsTitle: "Celkové statistiky",
+    thankYouMessage: "Děkujeme za hlas!"
+};
