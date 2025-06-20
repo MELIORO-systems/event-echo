34b94167f8e54086ea9d7d3010e74e75d8c2d11a
MELIORO-systems <pavel@melioro.cz>
2025-06-21 00:16:37 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,21 @@
 const config = {
+    // Texty aplikace (zůstávají stejné)
     pageTitle: "Mini-aplikace: Měřič nálady",
     appTitle: "Měřič nálady",
     appSubtitle: "Pomozte nám sledovat aktuální náladu v komunitě!",
     buttonText: "Jak se teď cítíš?",
     modalTitle: "Jak se teď cítíš?",
     statsTitle: "Celkové statistiky",
-    thankYouMessage: "Děkujeme za hlas!"
+    thankYouMessage: "Děkujeme za hlas!",
+
+    // NOVÁ SEKCE: Vložte sem své údaje z Firebase projektu
+    // Tyto údaje získáte podle návodu v Části 2
+    firebaseConfig: {
+        apiKey: "AIzaSyBZVkwEynmCEFaxBv2b2B9FSqGYu4OLjUY",
+        authDomain: "mereni-nalady-web.firebaseapp.com",
+        projectId: "mereni-nalady-web",
+        storageBucket: "mereni-nalady-web.firebasestorage.app",
+        messagingSenderId: "478918382129",
+        appId: "1:478918382129:web:a7bb4c8833310a72a2bb10"
+    }
 };
